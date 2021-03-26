58928aa0cd30fbd5413d98a7e06e54b1ec5eb80d
wani <oen142@naver.com>
2021-03-26 16:38:41 +0900

    Initial commit

---
+++ src/main/java/com/wani/springjenkinscodedeploytutorial/SpringJenkinsCodedeployTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.springjenkinscodedeploytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsCodedeployTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsCodedeployTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/springjenkinscodedeploytutorial/SpringJenkinsCodedeployTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.springjenkinscodedeploytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsCodedeployTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
